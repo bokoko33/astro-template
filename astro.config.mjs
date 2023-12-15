c2117dda9588d5e85fe0c374692bffb89d56e0df
bokoko33 <work.bokoko33@gmail.com>
2023-12-16 01:02:49 +0900

    add swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
+import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
   },
-  vite: {
-    build: {
-      assetsInlineLimit: 0, // jsが<script>に直接展開されるのを防ぐ
-    },
-  },
+
+  integrations: [
+    swup({
+      // theme: false, //デフォルトで用意されている遷移アニメーションは使用しない
+      smoothScrolling: false, //スムーズスクロール無効化
+      updateBodyClass: true, //ページ遷移時にbody要素のクラス名を更新する
+      reloadScripts: false, //遷移後にスクリプトを再読み込みさせない
+    }),
+  ],
 });
